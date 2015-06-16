4b3775e98379c060f0a0a63e1bca4a455186e0a2
Derri <derri@live.ie>
2015-06-16 15:32:53 +0100

    Create Node.java
    A simple node class which is necessary for the implementation of the Huffman compression project.

---
+++ Node.java
@@ -0,0 +1,7 @@
+class Node
+{
+	public char letter;            //stores letter
+	public Node leftChild;         // this node's left child
+	public Node rightChild;        // this node's right child
+
+}
