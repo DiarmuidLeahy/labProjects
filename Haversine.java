57d968f2ecbc448f9cf2b467a68cd70369d1c41d
Derri <derri@live.ie>
2015-08-31 17:24:28 +0100

    Update Haversine.java

--- Haversine.java
+++ Haversine.java
@@ -19,12 +19,12 @@ public class Haversine
 	public static double dist(double lat1, double lon1, double lat2, double lon2)
 	{
 	   	double dLat = Math.toRadians(lat2 - lat1);
-	   	double dLon = Math.toRadians(lon2 - lon1);
-	    lat1 = Math.toRadians(lat1);
-	    lat2 = Math.toRadians(lat2);
+		double dLon = Math.toRadians(lon2 - lon1);
+	    	lat1 = Math.toRadians(lat1);
+	    	lat2 = Math.toRadians(lat2);
 	 
-	    double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
-	    double c = 2 * Math.asin(Math.sqrt(a));
-	    return R * c;
-    }
+	    	double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
+	    	double c = 2 * Math.asin(Math.sqrt(a));
+	    	return R * c;
+	 }
 }
